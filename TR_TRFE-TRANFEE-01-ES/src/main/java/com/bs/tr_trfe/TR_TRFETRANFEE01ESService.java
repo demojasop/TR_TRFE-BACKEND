9fc72ecea63bd263c29a8f2769dbd7993f673258
alejandro.blasco <alejandro.blasco@MW7YQF13AH91US.dir.svc.accenture.com>
2016-09-20 18:06:39 +0200

    s

--- TR_TRFE-TRANFEE-01-ES/src/main/java/com/bs/tr_trfe/TR_TRFETRANFEE01ESService.java
+++ TR_TRFE-TRANFEE-01-ES/src/main/java/com/bs/tr_trfe/TR_TRFETRANFEE01ESService.java
@@ -42,12 +42,18 @@ public class TR_TRFETRANFEE01ESService extends AbstractTR_TRFETRANFEE01ESService
 	private Double calculateFee(double amount, char isVip) {
 		// TODO Auto-generated method stub
 		Double f=0.0;
-		if (isVip=='1')
+		
+		if (isVip=='1'){
 			f=0.0;
-		else if (amount>1000.00)
-			f=amount*0.005; 
-		else 
-			f=amount*0.01;	
+		}	
+		else{
+			if (amount>1000.00){
+				f=amount*0.005; 
+			}
+			else {
+				f=amount*0.01;
+			}
+		}
 		return f;
 	}
 
