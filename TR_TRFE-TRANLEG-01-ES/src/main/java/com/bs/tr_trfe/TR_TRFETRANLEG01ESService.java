a345fbeacb4921ba2bd7da7baa2759080fce4b47
alejandro.blasco <alejandro.blasco@MW7YQF13AH91US.dir.svc.accenture.com>
2016-09-21 11:32:58 +0200

    externalizamos en fichero la lista de vips

--- TR_TRFE-TRANLEG-01-ES/src/main/java/com/bs/tr_trfe/TR_TRFETRANLEG01ESService.java
+++ TR_TRFE-TRANLEG-01-ES/src/main/java/com/bs/tr_trfe/TR_TRFETRANLEG01ESService.java
@@ -1,5 +1,9 @@
 package com.bs.tr_trfe;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,9 +20,28 @@ public class TR_TRFETRANLEG01ESService extends AbstractTR_TRFETRANLEG01ESService
 	@Override
 	public void execute() {
 		LOGGER.debug("Legacy Backend Data Client");
-		if(getIdclient().equalsIgnoreCase("00000000L"))
-			addError("TR_TRFE001", "ClientId is not correct");
-		setIsvip(LegacyBackend.isVIPClient(getIdclient()));
+		String client=getIdclient();
+		String isVip=isVIPClient(client);
+		
+		
+		setIsvip(isVip);
 	}
+
+	private String isVIPClient(String idclient) {
+		
+		String isVip="0";
+		
+		String vipUsers=getProperty("vipUsers");
+		
+		List<String> list = Arrays.asList(vipUsers.split(","));
+		if (list.contains(idclient))
+				isVip="1";
+		
+		
+		return isVip;
+		
+	}
+	
+	
 
 }
